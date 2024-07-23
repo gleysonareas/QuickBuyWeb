1e3890d3410284799e656a0dc4b1c1c52cbf9632
gleysonareas <gleysonareasdasilva@hotmail.com>
2024-07-23 21:34:28 +0000

    Update angular to v18

--- QuickBuyWeb/Startup.cs
+++ QuickBuyWeb/Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
@@ -30,7 +31,10 @@ namespace QuickBuyWeb
         {
             {
                 services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+                services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+
                 var connectionString = Configuration.GetConnectionString("MySqlConnection");
+
                 services.AddDbContext<QuickBuyContext>(option => option
                 .UseLazyLoadingProxies()
                 .UseMySql(connectionString, m => m.MigrationsAssembly("QuickBuyRepository")));
