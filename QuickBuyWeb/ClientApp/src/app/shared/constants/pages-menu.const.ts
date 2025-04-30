83338640d7bde72d5f3c0ed8996c27c1801e04b6
gleysonareas <gleyson.silva@liberfly.com.br>
2025-04-30 12:09:03 -0300

    refactor

--- QuickBuyWeb/ClientApp/src/app/shared/constants/pages-menu.const.ts
+++ QuickBuyWeb/ClientApp/src/app/shared/constants/pages-menu.const.ts
@@ -1,18 +1,28 @@
-import { IPagesMenu } from "../Model/core/pages-menu.interface";
+import { IPagesMenu } from "../interfaces/core/pages-menu.interface";
 
-export const PagesMenu: IPagesMenu[] = [{
-    name: 'Home',
-    listLink: ''
-}, {
-    name: 'Couter',
-    listLink: '/counter'
-}, {
-    name: 'Fetch data',
-    listLink: '/fetch-data'
-}, {
-    name: 'Produto',
-    listLink: '/produto',
-}, {
-    name: 'Login',
-    listLink: '/login',
-}]
+export const PagesMenu: IPagesMenu[] = [
+    {
+        name: 'Home',
+        listLink: ''
+    },
+    {
+        name: 'Couter',
+        listLink: '/counter'
+    },
+    {
+        name: 'Fetch data',
+        listLink: '/fetch-data'
+    },
+    {
+        name: 'Produto',
+        listLink: '/produto',
+    },
+    {
+        name: 'Loja',
+        listLink: '/store',
+    },
+    {
+        name: 'Login',
+        listLink: '/login',
+    },
+]
