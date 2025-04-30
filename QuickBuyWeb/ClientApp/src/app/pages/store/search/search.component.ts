f2e09749c684bea3b447fdfefbcce0e09bf027f4
gleysonareas <gleyson.silva@liberfly.com.br>
2025-04-30 12:06:50 -0300

    Implements search component

--- QuickBuyWeb/ClientApp/src/app/pages/store/search/search.component.ts
+++ QuickBuyWeb/ClientApp/src/app/pages/store/search/search.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ProductService } from "../../../core/services/product.service";
-import { IProduct } from "../../../shared/model/product.interface";
+import { IProduct } from "../../../shared/interfaces/product.interface";
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-search',
-  standalone: true,
-  imports: [],
+  // standalone: true,
+  // imports: [],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
@@ -13,6 +14,7 @@ import { IProduct } from "../../../shared/model/product.interface";
 export class SearchComponent implements OnInit {
 
   private productService = inject(ProductService);
+  private router = inject(Router);
 
   public products: IProduct[] = [];
 
@@ -21,5 +23,11 @@ export class SearchComponent implements OnInit {
       next: (resp) => { this.products = resp },
       error: (err) => { console.log(err); }
     })
+  }
+
+  openProduct(product: IProduct) {
+    sessionStorage.removeItem('productDetail');
+    sessionStorage.setItem('productDetail', JSON.stringify(product));
+    this.router.navigate(['/store']);
   }
 }
